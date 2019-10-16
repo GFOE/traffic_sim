d41ae1f476f892ffeccd31da827296ed64d04dd3
Roland Arsenault <roland@ccom.unh.edu>
2019-10-16 15:05:28 -0400

    Made joystick controlled targets spawn closer to asv.

--- nodes/traffic_sim_node.py
+++ nodes/traffic_sim_node.py
@@ -15,19 +15,26 @@ joy_vessels = {}
 
 target_vessel_count = 5
 
-new_vessel_range = (500,5000)
-vessel_delete_range = 10000
+new_vessel_range = (250,1000)
+vessel_delete_range = 3000
 vessel_speed_range = (2,10) # m/s
 next_id = 1
 
 contact_pub = None
 
 class Vessel:
-    def __init__(self,start_time=None):
+    def __init__(self,start_time=None, joy_topic=None):
         global next_id
         self.last_time = start_time
-        distance =  random.uniform(new_vessel_range[0],new_vessel_range[1])
-        bearing = random.uniform(0,360)
+        if joy_topic is None:
+            distance =  random.uniform(new_vessel_range[0],new_vessel_range[1])
+            bearing = random.uniform(0,360)
+        else:
+            distance = 100
+            joy_id = int(joy_topic[-1])
+            print 'joy_id',joy_id
+            bearing = -45.0 + 90*joy_id
+                
         self.position = project11.geodesic.direct(math.radians(asv_position.position.longitude),math.radians(asv_position.position.latitude),math.radians(bearing),distance)
         
         # itinitate a random heading that generally points towards the vehicle
@@ -39,18 +46,43 @@ class Vessel:
             self.last_report_time = None
         else:
             self.last_report_time = start_time + rospy.Duration.from_sec(random.uniform(-10.0,0.0))
-        self.report_rate = 10.0
         
-        self.target_id = 'sim{:04}'.format(next_id)
+
+        if joy_topic is not None:
+            rospy.Subscriber(joy_topic, Joy, self.joystickCallback)
+            self.target_id = joy_topic
+            self.report_rate = 0.25
+        else:
+            self.target_id = 'sim{:04}'.format(next_id)
+            self.report_rate = 10.0
+
         self.mmsi = next_id
         next_id += 1
         
         self.speed = random.uniform(vessel_speed_range[0],vessel_speed_range[1])
+        
+        self.throttle = None
+        self.rudder = None
+        
+        self.max_speed = 8.0
+        self.min_speed = 0.0
+        self.speed_change_rate = 1.5
     
     def iterate(self, data):
         if self.last_time is not None:
             delta_t = data.current_real - self.last_time
             if delta_t > rospy.Duration(0):
+                if self.throttle is not None:
+                    target_speed = max(self.min_speed,self.throttle*self.max_speed)
+                    if target_speed > self.speed:
+                        delta_speed = self.speed_change_rate*delta_t.to_sec()
+                        self.speed = min(self.speed+delta_speed,target_speed)
+                    else:
+                        delta_speed = -self.speed_change_rate*delta_t.to_sec()
+                        self.speed = max(self.speed+delta_speed,target_speed)
+                if self.rudder is not None:
+                    self.rate_of_turn = self.rudder
+                        
                 distance = delta_t.to_sec()*self.speed
                 self.heading += self.rate_of_turn*delta_t.to_sec()
                 self.position = project11.geodesic.direct(self.position[0],self.position[1],self.heading,distance)
@@ -78,6 +110,23 @@ class Vessel:
         b,d = project11.geodesic.inverse(math.radians(asv_position.position.longitude),math.radians(asv_position.position.latitude),self.position[0],self.position[1])
         return d
 
+    def joystickCallback(self,data):
+        self.throttle = data.axes[1]
+        self.rudder = -data.axes[3]
+        if data.buttons[8]:
+            self.throttle = 0.0
+            self.rudder = 0.0
+            
+            distance = 100
+            joy_id = int(self.target_id[-1])
+            bearing = -45.0 + 90*joy_id
+                
+            self.position = project11.geodesic.direct(math.radians(asv_position.position.longitude),math.radians(asv_position.position.latitude),math.radians(bearing),distance)
+            self.heading = math.radians(bearing)
+
+            
+
+
 def iterate(data):
     to_delete = []
     for v in vessels:
@@ -94,23 +143,16 @@ def iterate(data):
         while len(vessels) < target_vessel_count:
             v = Vessel(data.current_real)
             vessels.append(v)
+        if len(joy_vessels) < 4:
+            for i in range(4):
+                topic = '/joy_'+str(i)
+                joy_vessels[topic] = Vessel(None,topic)
+                
         
 
 def positionCallback(data):
     global asv_position
     asv_position = data
-
-def joystickCallback(data):
-    global joy_vessels
-    if asv_position is not None:
-        topic =  data._connection_header['topic']
-        if not topic in joy_vessels:
-            joy_vessels[topic] = Vessel()
-            joy_vessels[topic].target_id = topic
-            joy_vessels[topic].report_rate = 0.1
-        joy_vessels[topic].speed = data.axes[1]*20.0
-        joy_vessels[topic].rate_of_turn = -data.axes[3]
-    
 
 if __name__ == '__main__':
     rospy.init_node('traffic_sim')
@@ -118,9 +160,6 @@ if __name__ == '__main__':
     contact_pub = rospy.Publisher('/contact',Contact,queue_size = 10)
     
     rospy.Subscriber('/position', GeoPointStamped, positionCallback)
-    
-    for i in range(10):
-        rospy.Subscriber('/joy_'+str(i), Joy, joystickCallback)
     
     rospy.Timer(rospy.Duration.from_sec(0.2),iterate)
     rospy.spin()
